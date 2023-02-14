86e99d8cf12bea5b26fb4377440a527ffd1a742e
FrontEndDanni <dannikabdull@gmail.com>
2023-02-14 00:58:41 -0800

    UI changes, added password regex

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,19 +3,29 @@ import "./login.scss";
 import Container from 'react-bootstrap/Container';
 import {Link} from "react-router-dom";
 
-
 export default function Login() {
   return (
     <>
-        <div className="login">
-            <div className="loginCard">
-                <div className="loginContent">
-                    <h1>Login</h1>
+        <div className="register">
+            <div className="registerCard">
+                <div className="registerContent">
+                    <h1>Welcome back!</h1>
                     <form>
-                        <Link to="#">
-                            <Button>Login</Button>
-                        </Link>
+                        <label>
+                            Email:
+                            <input type="email" className="form-control"/>
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" className="form-control"/>
+                        </label>
+                        <sup>Forgot your password?</sup>
+                            <Link to="#">
+                                 <Button >Login</Button>
+                            </Link>
+                            <h5>Don't have an account? <Link to="/register">Register here!</Link></h5>
                     </form>
+                
                 </div>
 
             </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,40 +1,54 @@
-import { Button,Stack } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Stack } from 'react-bootstrap';
 import "./register.scss";
 import Container from 'react-bootstrap/Container';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Register() {
+  const [password, setPassword] = useState('');
+
+  function validatePassword() {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+    return passwordRegex.test(password);
+  }
+
   return (
     <>
-        <div className="register">
-            <div className="registerCard">
-                <div className="registerContent">
-                    <h1>Register</h1>
-                    <form>
-                        <label>
-                            First Name:
-                            <input type="text" className="form-control"/>
-                        </label>
-                        <label>
-                            Last Name:
-                            <input type="text" className="form-control"/>
-                        </label>
-                        <label>
-                            Email:
-                            <input type="email" className="form-control"/>
-                        </label>
-                        <label>
-                            Password:
-                            <input type="password" className="form-control"/>
-                        </label>
-                        <Link to="#">
-                            <Button>Register</Button>
-                        </Link>
-                    </form>
+      <div className="register">
+        <div className="registerCard">
+          <div className="registerContent">
+            <h1>Create your Account</h1>
+            <form>
+              <label>
+                Username:
+                <input type="text" className="form-control" />
+              </label>
+              <label>
+                Email:
+                <input type="email" className="form-control" />
+              </label>
+              <label>
+                Password:
+                <input type="password" label="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </label>
+              {password.length > 0 &&
+                <div className={validatePassword() ? 'validPassword' : 'invalidPassword'}>
+                  {validatePassword() ? 'Password is secure!' : 'Password must be at least 8 characters with at least one capital letter, one number, and one punctuation mark.'}
                 </div>
+              }
+              <center>
+                <Link to="#">
+                  <Button>Register</Button>
+                </Link>
+                <br></br>
+                <br></br>
+                <h5>Already have an account? <Link to="/login">Log in!</Link></h5>
+              </center>
+            </form>
+          </div>
 
-            </div>
         </div>
+      </div>
 
     </>
   )
